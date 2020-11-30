a8796ed2b04315f6f3e79541650e30e5b885483c
BUNTU <peter5353@naver.com>
2020-11-30 12:26:17 +0900

    OnlineClassAlarmSender
    Send subject notification to KaKaoTalk.

---
+++ OnlineClassAlarm/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace OnlineClassAlarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(8, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(190, 21);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "카카오톡 채팅방 리스트";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(289, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "알림시작";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 102);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(186, 143);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(208, 133);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 25);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "추가";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(208, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 25);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "삭제";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 102);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(156, 21);
+            this.textBox1.TabIndex = 10;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 165);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 25);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "알림종료";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9.749999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(204, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 14);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "알림이 시작되었습니다.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 259);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Font = new System.Drawing.Font("나눔스퀘어라운드 Regular", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "OnlineClassAlarm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ OnlineClassAlarm/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using MaterialSkin;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineClassAlarm
+{
+    public partial class Form1 : MaterialForm
+    {
+        [DllImport("user32.dll")]
+        public static extern int FindWindow(string lpClassName, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern int FindWindowEx(int hWnd1, int hWnd2, string lpsz1, string lpsz2);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(int hwnd, int wMsg, int wParam, string lParam);
+
+        [DllImport("user32.dll")]
+        public static extern uint PostMessage(int hwnd, int wMsg, int wParam, int lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public string[] tableNum;
+        public string[] setting;
+        public string[,] finalTable = new string[7, 10];
+        public string[] timeTable = new string[10];
+        public string tablePath = System.Windows.Forms.Application.StartupPath + @"\table.csv";
+        public string iniPath = System.Windows.Forms.Application.StartupPath + @"\packages\setting.ini";
+        public bool[] check = new bool[7];
+        public List<string> roomList = new List<string>();
+        public void readTable()
+        {
+
+            if (File.Exists(tablePath))
+            {
+                tableNum = System.IO.File.ReadAllLines(tablePath, Encoding.Default);
+                
+            }
+            else
+            {
+                SystemSounds.Beep.Play();
+                MessageBox.Show("table.csv를 작성해주세요.", "OnlineClassAlarm", MessageBoxButtons.OK);
+                Environment.Exit(0);
+            }
+            string[] temp = tableNum[0].Split(',');
+            for (int j = 0; j < temp.Length - 1; j++)
+            {
+                for (int i = 0; i < tableNum.Length - 1; i++)
+                {
+                    string[] temp2 = tableNum[i + 1].Split(',');
+                    finalTable[j, i] = temp2[j];
+                }
+            }
+            for (int i = 0; i < tableNum.Length - 1; i++)
+            {
+                string[] temp2 = tableNum[i + 1].Split(',');
+                timeTable[i] = temp2[temp.Length - 1];
+            }
+        }
+        public void readSetting()
+        {
+            if (File.Exists(iniPath))
+            {
+                setting = System.IO.File.ReadAllLines(iniPath, Encoding.Default);
+                for (int i = 0; i < setting.Length; i++)
+                {
+                    var row = new string[] { setting[i] };
+                    var item = new ListViewItem(row);
+                    listView1.Items.Add(item);
+                    roomList.Add(setting[i]);
+                }
+            }
+            else
+            {
+                System.IO.File.WriteAllText(iniPath, "", Encoding.Default);
+            }
+        }
+        public void sendMessage(string windowName, string message)
+        {
+            int hd01 = FindWindow(null, windowName);
+            int hd03 = FindWindowEx(hd01, 0, "RichEdit50W", "");
+            if (!hd03.Equals(IntPtr.Zero))
+            {
+                SendMessage(hd03, 0x000c, 0, message);
+                PostMessage(hd03, 0x0100, 0xD, 0x1C001);
+            }
+        }
+        public void getTime(int day)
+        {
+            DateTime time1 = DateTime.Now;
+            for (int i = 0; i < tableNum.Length - 1; i++)
+            {
+                string[] timeTemp = timeTable[i].Split(':');
+                TimeSpan gap = time1 - new DateTime(time1.Year, time1.Month, time1.Day, Convert.ToInt32(timeTemp[0]), Convert.ToInt32(timeTemp[1]), 0);
+                if ((int)gap.TotalSeconds <= 10 && (int)gap.TotalSeconds >= 0)
+                {
+                    if (!(string.IsNullOrEmpty(roomList[0])))
+                    {
+                        int temp = i + 1;
+                        if (!(string.IsNullOrEmpty(finalTable[day, i])))
+                        {
+                            if (check[i] != true)
+                            {
+                                readTable();
+                                this.Invoke(new Action(
+                                delegate ()
+                                {
+                                    label1.Text = temp + "교시 " + finalTable[day, i] + "시간입니다.";
+                                }));
+                                foreach (string s in roomList)
+                                {
+                                    sendMessage(s, temp + "교시 " + finalTable[day, i] + "시간입니다.");
+                                }
+                                SystemSounds.Beep.Play();
+                                MessageBox.Show(temp + "교시 " + finalTable[day, i] + "시간입니다.", "OnlineClassAlarm", MessageBoxButtons.OK);
+                                check[i] = true;
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            readTable();
+            readSetting();
+            label1.Text = "알림이 시작되지 않았습니다.";
+        }
+        private void callMethod()
+        {
+            while (true)
+            {
+                DateTime dt = DateTime.Now;
+                var day = dt.DayOfWeek;
+                switch (day)
+                {
+                    case DayOfWeek.Monday:
+                        getTime(0);
+                        break;
+                    case DayOfWeek.Tuesday:
+                        getTime(1);
+                        break;
+                    case DayOfWeek.Wednesday:
+                        getTime(2);
+                        break;
+                    case DayOfWeek.Thursday:
+                        getTime(3);
+                        break;
+                    case DayOfWeek.Friday:
+                        getTime(4);
+                        break;
+                    default:
+                        break;
+                }
+                Thread.Sleep(3000);
+            }
+        }
+        public Thread t1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "알림이 시작되었습니다.";
+            t1 = new Thread(callMethod);
+            t1.Start();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (!(string.IsNullOrEmpty(textBox1.Text)))
+            {
+                var row = new string[] { textBox1.Text };
+                var item = new ListViewItem(row);
+                listView1.Items.Add(item);
+                roomList.Add(textBox1.Text);
+                if (File.Exists(iniPath))
+                {
+                    System.IO.File.Delete(iniPath);
+                    setting = roomList.ToArray();
+                    System.IO.File.WriteAllLines(iniPath, setting, Encoding.Default);
+                }
+                else if (!(File.Exists(iniPath)))
+                {
+                    setting = roomList.ToArray();
+                    System.IO.File.WriteAllLines(iniPath, setting, Encoding.Default);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listView1.SelectedItems.Count == 1)
+            {
+                int index = listView1.SelectedItems[0].Index;
+                listView1.Items.RemoveAt(index);
+                roomList.RemoveAt(index);
+                if (File.Exists(iniPath))
+                {
+                    System.IO.File.Delete(iniPath);
+                    setting = roomList.ToArray();
+                    System.IO.File.WriteAllLines(iniPath, setting, Encoding.Default);
+                }
+                else if (!(File.Exists(iniPath)))
+                {
+                    setting = roomList.ToArray();
+                    System.IO.File.WriteAllLines(iniPath, setting, Encoding.Default);
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (t1 != null)
+            {
+                t1.Abort();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (t1 != null)
+            {
+                label1.Text = "알림이 시작되지 않았습니다.";
+                t1.Abort();
+            }
+        }
+    }
+}
